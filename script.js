4b4103e97882bb5755867122969c2450b98d83ed
Emil Åkerman <emil.akerman@iths.se>
2023-03-24 12:01:36 +0100

    a bit of design work on the card view

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ body.appendChild(weatherCard);
 //api stuff current one
 let inputCity = '';
 // let apiUrl = `http://api.weatherapi.com/v1/current.json?key=4b00f66c64844ff0a65120230232303&q=${inputCity}`;
-//new api stuff
-//key 6395fbd333a047e2d87d11df8ddf03ac
-//test with this http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6395fbd333a047e2d87d11df8ddf03ac
+
 
 searchButton.addEventListener('click', () => {
     inputCity = inputField.value;
@@ -30,9 +28,9 @@ const fetchThis = (inputCity) => {
     .then((data) => {
         weatherImg.setAttribute('src', data.current.condition.icon);
         weatherImg.setAttribute('alt', data.current.condition.text);
-        cityName.innerText = `${data.location.name} (${data.location.country})`;
-        weatherCondition.innerText = data.current.condition.text;
-        temperatureText.innerText = `${data.current.temp_c}°C (${data.current.temp_f}°F)`;
+        cityName.innerText = `${data.location.name}, ${data.location.country}`;
+        // weatherCondition.innerText = data.current.condition.text;
+        temperatureText.innerText = `${data.current.temp_c}°C`;
         inputCity = data.location.name;
     });
 }
